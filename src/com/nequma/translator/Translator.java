8f51e66591015f470ff59dfbeb2c36f09237e7a1
yegor <yegorgagarin@mail.ru>
2026-02-18 00:59:23 +0500

    Added flags of running

--- src/com/nequma/translator/Translator.java
+++ src/com/nequma/translator/Translator.java
@@ -9,26 +9,42 @@ import java.util.List;
 public class Translator {
     static boolean hadError = false;
     static boolean hadRuntimeError = false;
+    static boolean scanningOutExpected;
+    static boolean parsingOutExpected;
+    static boolean viewStagesExpected;
 
     public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: translator [script]");
+        if (args.length < 1 || args.length > 4) {
+            System.out.println("Usage: java Translator <path> [-VS] [-SO] [-PO]");
             System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
         }
+
+        scanningOutExpected = false;
+        parsingOutExpected = false;
+        viewStagesExpected = false;
+
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case "-VS":
+                    viewStagesExpected = true;
+                    break;
+                case "-SO":
+                    scanningOutExpected = true;
+                    break;
+                case "-PO":
+                    parsingOutExpected = true;
+                    break;
+                default:
+                    System.err.println("Unknown flag: " + args[i]);
+            }
+        }
+        runFile(args[0]);
     }
 
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         String source = new String(bytes, Charset.defaultCharset());
 
-        System.out.println("\n+Source language program:");
-        System.out.println(source);
-
-        System.out.println("\n+Scanning output:");
         run(source);
 
         if (hadError) System.exit(65);
@@ -51,24 +67,29 @@ public class Translator {
 
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scanTokens();
-
-        // Вывод токенов
-        StringBuilder outString = new StringBuilder();
-        for (Token token : tokens) {
-            System.out.println(token);
-            outString.append(token.toString()).append("\n");
+        if (viewStagesExpected){
+            System.out.println("Source language:");
+            System.out.println(source);
         }
 
-        // Сохранение вывода сканера в файл
-        try {
-            File outFile = new File("ScannerOutput.txt");
-            try (PrintWriter fileWriter = new PrintWriter(outFile)) {
-                fileWriter.write(outString.toString());
+        List<Token> tokens = scanner.scanTokens();
+
+        if (viewStagesExpected || scanningOutExpected) {
+            StringBuilder outString = new StringBuilder();
+            if (viewStagesExpected) System.out.println("\nScanning output:");
+            for (Token token : tokens) {
+                if (viewStagesExpected) System.out.println(token.toString());
+                outString.append(token.toString()).append("\n");
             }
-            System.out.println("\nScanner output saved to ScannerOutput.txt");
-        } catch (FileNotFoundException ex) {
-            System.out.println(ex.getMessage());
+            if (scanningOutExpected) {
+                try {
+                    try (PrintWriter fileWriter = new PrintWriter("ScannerOutput.txt")) {
+                        fileWriter.write(outString.toString());
+                    }
+                } catch (FileNotFoundException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
         }
 
         if (hadError) {
@@ -76,23 +97,37 @@ public class Translator {
             return;
         }
 
-        System.out.println("\nParsing output:");
+        //System.out.println("Scanning successful.");
+
         Parser parser = new Parser(tokens);
         List<Stmt> statements = parser.parse();
+
+        if (viewStagesExpected || parsingOutExpected) {
+            AstPrinter printer = new AstPrinter();
+            StringBuilder outString = new StringBuilder();
+            if (viewStagesExpected) System.out.println("\nParsing output:");
+            for (Stmt stmt : statements) {
+                if (viewStagesExpected) System.out.println(printer.print(stmt));
+                outString.append(printer.print(stmt)).append("\n");
+            }
+            if (parsingOutExpected) {
+                try {
+                    try (PrintWriter fileWriter = new PrintWriter("ParserOutput.txt")) {
+                        fileWriter.write(outString.toString());
+                    }
+                } catch (FileNotFoundException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
 
         if (parser.hadError() || hadError) {
             System.out.println("Parsing failed.");
             return;
         }
 
-        System.out.println("Parsing successful!");
-        System.out.println("Number of statements: " + statements.size());
-
-        // Вывод структуры программы (для отладки)
-        AstPrinter printer = new AstPrinter();
-        for (Stmt stmt : statements) {
-            System.out.println(printer.print(stmt));
-        }
+        //System.out.println("Parsing successful.");
+        //System.out.println("Number of statements: " + statements.size());
 
         System.out.println("\nInterpreter output:");
         Interpreter interpreter = new Interpreter();
